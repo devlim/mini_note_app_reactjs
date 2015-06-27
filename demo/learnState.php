20104854c1e55ac83af926788e6a9c3633003aa1
devlim <devlimke@gmail.com>
2015-06-28 07:32:48 +0800

    move learnState.php to demo folder

--- demo/learnState.php
+++ demo/learnState.php
@@ -1,10 +1,9 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<link rel="stylesheet" href="lib/css/bootstrap.min.css">
-		<link rel="stylesheet" href="public/css/style.css">
-		<script type="text/javascript" src="lib/js/react-0.13.3.js"></script>
-		<script type="text/javascript" src="lib/js/JSXTransformer-0.13.3.js"></script>
+		<link rel="stylesheet" href="../lib/css/bootstrap.min.css">
+		<script type="text/javascript" src="../lib/js/react-0.13.3.js"></script>
+		<script type="text/javascript" src="../lib/js/JSXTransformer-0.13.3.js"></script>
 	</head>
 	<body>
 		<div class="container">
