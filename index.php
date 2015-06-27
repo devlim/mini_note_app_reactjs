89317e1efece8b4694f2bdde038e5ab6ee65f694
devlim <devlimke@gmail.com>
2015-06-28 07:17:11 +0800

    update Note component

--- index.php
+++ index.php
@@ -2,12 +2,17 @@
 <html>
 	<head>
 		<link rel="stylesheet" href="lib/css/bootstrap.min.css">
-		<script type="text/javascript" src="lib/js/react-0.13.3.min.js"></script>
+		<link rel="stylesheet" href="public/css/style.css">
+		<script type="text/javascript" src="lib/js/react-0.13.3.js"></script>
 		<script type="text/javascript" src="lib/js/JSXTransformer-0.13.3.js"></script>
 	</head>
 	<body>
 		<div class="container">
+			<div class="row">
+				<div class="col-md-12 content-container">
 
+				</div>
+			</div>
 		</div>
 		<script type="text/jsx" src="public/js/note.js">
 			
--- public/js/note.js
+++ public/js/note.js
@@ -1,12 +1,24 @@
 var Note = React.createClass({
+	remove: function(){
+		console.log("delete note");
+	},
+	edit: function(){
+		console.log("editing text");
+	},
 	render: function(){
 		return (
-			<div className="note">
-				Note
+			<div className="note col-md-2">
+				<p>
+					{this.props.children}
+				</p>
+				<span>
+					<button className="btn btn-primary glyphicon glyphicon-pencil" onClick={this.edit}></button>
+					<button className="btn btn-danger glyphicon glyphicon-trash" onClick={this.remove}></button>
+				</span>
 			</div>
 			)
 	}
 });
 
-React.render(<Note />, document.querySelector('.container'));
+React.render(<Note>empty note</Note>, document.querySelector('.content-container'));
 
