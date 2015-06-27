6f47dd6acc6bfa63b5602119877e8c29f45a163c
devlim <devlimke@gmail.com>
2015-06-28 07:56:10 +0800

    not follow the video tutorial

--- public/js/note.js
+++ public/js/note.js
@@ -1,15 +1,42 @@
 var Note = React.createClass({
+	getInitialState: function(){
+		return {
+			editing:false,
+			text: this.props.children
+		}
+	},
 	remove: function(){
 		console.log("delete note");
 	},
 	edit: function(){
 		console.log("editing text");
+		this.setState({
+			editing: !this.state.editing
+		});
 	},
-	render: function(){
+	update: function(){
+		this.setState({
+			editing: !this.state.editing,
+			text: this.refs.newText.getDOMNode().value
+		});
+	},
+	displayEdit: function(){
+		return (
+			<div className="note col-md-2">
+				<textarea ref="newText" class="form-group">
+					{this.state.text}
+				</textarea>
+				<span>
+					<button className="btn btn-success glyphicon glyphicon-floppy-disk" onClick={this.update}></button>
+				</span>
+			</div>
+			)
+	},
+	displayNonEdit: function(){
 		return (
 			<div className="note col-md-2">
 				<p>
-					{this.props.children}
+					{this.state.text}
 				</p>
 				<span>
 					<button className="btn btn-primary glyphicon glyphicon-pencil" onClick={this.edit}></button>
@@ -17,6 +44,13 @@ var Note = React.createClass({
 				</span>
 			</div>
 			)
+	},
+	render: function(){
+		if(this.state.editing){
+			return this.displayEdit();
+		}else{
+			return this.displayNonEdit();
+		}
 	}
 });
 
